e19a8bece9a6d5664a0a616ca8fd614cf2054d29
MOHAMMAD SHAIBAZ QAISAR <mohammad.shaibaz.qaisar4@gmail.com>
2020-12-25 16:20:43 +0530

    intial commit

---
+++ Forloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Forloop {
+
+pubic static void main(String args[]){
+
+for(int i =0;i<10;i++)
+{
+System.out.println(i);
+}
+
+}
+
+}
---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+import java.util;
+class Helloworld{
+public static void main(String[] args) {
+System.out.println("Hello Shaibaz");
+
+}
+
+
+}
